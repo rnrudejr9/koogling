eae02c535bbde4eb28483af842b345324907bd04
Koogling <rnrudejr1234@naver.com>
2022-07-06 17:22:19 +0900

    myhomepage(ver1)
    html + css + js
    1. 버튼이벤트 및 토글
    2. 레이아웃구조

---
+++ test.js
@@ -0,0 +1,10 @@
+
+function changeDiv(self){
+    var div = self;
+    if(div.style.display === 'none'){
+        div.style.display = 'block';
+    }else{
+        div.style.display = 'none';
+    }
+}
+
