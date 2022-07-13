f22b57f4824d9e3e47f57a0325eb172a024e9704
Koogling <rnrudejr1234@naver.com>
2022-07-13 23:09:30 +0900

    index update
    메인화면수정
    사진 변경
    검색창 구현중

--- test.js
+++ test.js
@@ -8,5 +8,12 @@ function openRank(){
     
 }
 
+function search(self){
+    var div = self
+    var inf = document.getElementById("nav_input").value;
+
+    div.style.display= window.location.href = "http:naver.com";
+   
+}
 
 
