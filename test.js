062b56e4f353a0393719a27865f5586620a18434
Koogling <rnrudejr1234@naver.com>
2022-07-13 16:57:43 +0900

    add background
    백그라운드 추가
    단순 수정

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 
 function changeDiv(self){
     var div = self;
-    if(div.style.display === 'none'){
-        div.style.display = 'block';
-    }else{
-        div.style.display = 'none';
-    }
+    div.style.display = 'block';
 }
+
+function openRank(){
+    
+}
+
+
 
